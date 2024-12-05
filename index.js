16762f4c102d559bb1c0d4ea1c7800e366f43546
Ayomide00712 <usmanalkassim01@gmail.com>
2024-12-04 18:20:50 -0800

    modified

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+
 var btn = document.querySelector("#click");
 var show = document.querySelector(".media");
+var dropdownItems = document.querySelectorAll(".items"); // Select all dropdown items
 
-btn.addEventListener("click", function(){
-    // console.log("owk");
-    show.classList.toggle('show');
-})
+btn.addEventListener("click", function () {
 
+    show.classList.toggle("show");
+});
 
-function MydropDowns(clicked){
-   
+function MydropDowns(clicked) {
+    dropdownItems.forEach(item => {
+        var child = item.children[0]; 
+        if (item !== clicked && child.classList.contains("open")) {
+            child.classList.remove("open");
+        }
+    });
+
+    // Toggle the current dropdown
     var gotten = clicked.children[0];
-    gotten.classList.toggle('open');
- }
+    gotten.classList.toggle("open");
+}
